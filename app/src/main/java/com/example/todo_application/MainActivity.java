223aacbb96692dea556311e2f63fa0aa9114abbb
Yash1943 <joshiyash1942003@gmail.com>
2024-02-11 06:57:09 +0530

    RecylerView and Creating Model

--- app/src/main/java/com/example/todo_application/MainActivity.java
+++ app/src/main/java/com/example/todo_application/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.todo_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView taskRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        taskRecyclerView = findViewById(R.id.taskRecycleView);
+        taskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/todo_application/Model/ToDoModel.java
@@ -0,0 +1,4 @@
+package com.example.todo_application.Model;
+
+public class ToDoModel {
+}
